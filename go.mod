30f9d34e8dec54a5ba0540b255f02d50c67dacef
s-cod <89507850000s@gmail.com>
2023-03-31 09:09:05 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module create_bat
+
+go 1.20
+
+require github.com/gelsrc/go-charset v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// import "github.com/gelsrc/go-charset"
+func main() {
+
+	filesIn, _ := os.ReadDir("./")
+	var cer_file []string
+
+	for i := range filesIn {
+		name := filesIn[i].Name()
+		if name[len(name)-3:] == "cer" {
+			cer_file = append(cer_file, name)
+		}
+	}
+
+	batFile, err := os.Create("install_cer.bat")
+	if err != nil {
+		fmt.Println("Ошибка создагтя bat файла")
+	}
+	defer func(f *os.File) {
+		f.Close()
+	}(batFile)
+
+	batFile.WriteString("chcp 65001 >nul\r\n")
+	// batFile.Write(charset.Cp1251RunesToBytes([]rune("chcp 1251 >nul\n")))
+	// batFile.Write(charset.886 .Cp1251RunesToBytes([]rune("chcp 1251 >nul\n")))
+	for i := range cer_file {
+		f := fmt.Sprintf("csptest -ipsec -reg -autocont -mycert \"./%s\"\r\n", cer_file[i])
+		// win := charset.Cp1251RunesToBytes([]rune(f))
+		fmt.Println(f)
+		// fmt.Println(win)
+		batFile.WriteString(f)
+	}
+}
